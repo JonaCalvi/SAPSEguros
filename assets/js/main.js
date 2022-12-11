820675ad456db0ffcb5fd4efec68779be676e9aa
JonaCalvi <jonacalvi.trabajo@gmail.com>
2022-12-11 13:36:30 -0300

    cambios finales

--- assets/js/main.js
+++ assets/js/main.js
@@ -272,7 +272,7 @@ const formulario = document.querySelector('#formulario');
 const buttonSubmit = document.querySelector('#submit');
 const urlDesktop = 'https://web.whatsapp.com/';
 const urlMobile = 'whatsapp://';
-const telefono = '+5402612090679';
+const telefono = '+5402616621660';
 
 formulario.addEventListener('submit', (event) => {
     event.preventDefault()
